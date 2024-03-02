7926efaa1d8848faea48ca69324966668aad49d1
HessamCheraghi <hessam.ch.rose@gmail.com>
2024-03-02 17:56:47 +0330

    4th iteration for github pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/hpcan-homepage",
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
